7c57a3386342c58e5cbd7b3c7dc9019b2f77afbe
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2024-08-19 17:36:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-// Replace with your Google Sheets API key and Spreadsheet ID
 const API_KEY = 'AIzaSyA4SnI-q5SjQk_g1L-3yCE0yTLu_8nob8s';
 const SPREADSHEET_ID = '1tr9EYkquStJozfVokqS1Ix_Ugwn7xfhUX9eOu6x5WEE';
-const RANGE = 'Sheet1!A2:H'; // Adjust the range to your needs
+const RANGE = 'Sheet1!A2:H'; // Adjust the range to include Host, Phone Number, Email
 
 function fetchListings() {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
 
     fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
             const listings = data.values;
             displayListings(listings);
         })
         .catch(error => console.error('Error fetching data:', error));
 }
-
 
 function displayListings(listings) {
     const listingsContainer = document.getElementById('listings');
