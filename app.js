c7f2ae0d84e5d998c944683e1f6496d4880c5b13
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2024-08-19 17:31:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Replace with your Google Sheets API key and Spreadsheet ID
 const API_KEY = 'AIzaSyA4SnI-q5SjQk_g1L-3yCE0yTLu_8nob8s';
 const SPREADSHEET_ID = '1tr9EYkquStJozfVokqS1Ix_Ugwn7xfhUX9eOu6x5WEE';
-const RANGE = 'Sheet1!A2:E'; // Adjust the range to your needs
+const RANGE = 'Sheet1!A2:H'; // Adjust the range to your needs
 
 function fetchListings() {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
