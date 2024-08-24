d84d9a083c354ae0e991f4baee40779c0c66d9ac
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2024-08-24 18:41:32 +0700

    Update rental.js

--- rental.js
+++ rental.js
@@ -1,3 +1,4 @@
+//hosting on Github doesn't have any way to hide the API key so here it is, insecure code so please don't replicate or abuse it, thank you.
 const API_KEY = 'AIzaSyA4SnI-q5SjQk_g1L-3yCE0yTLu_8nob8s';
 const SPREADSHEET_ID = '1tr9EYkquStJozfVokqS1Ix_Ugwn7xfhUX9eOu6x5WEE';
 const RANGE = 'Sheet1!A2:I'; // Adjust range to include District column
